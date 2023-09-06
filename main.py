f8b4a00c586cf62d8ebc5ddeae27dfe7cd582011
Nikita <Narchuk96@gmail.com>
2023-09-06 15:17:11 +0300

    test

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pytest
+from selenium import webdriver
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-print('This is a new one test')
+driver = webdriver.Chrome (executable_path="C:\\Users\\Nikita\\PycharmProjects\\Selenium\\chrome_driver\\chromedriver.exe")
