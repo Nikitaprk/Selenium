92f3c24cd2f80a098f2096922db3babf8e7935bb
Nikita <Narchuk96@gmail.com>
2023-09-06 18:54:16 +0300

    test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import time
-import pytest
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-url = 'https://www.vk.com'
-
+url = 'https://www.ozon.ru'
 driver.get(url=url)
 time.sleep(3)
 driver.close()
