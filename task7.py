37d2c2f8919470717be277f006a502a347241df6
Nikita <Narchuk96@gmail.com>
2023-09-17 22:33:11 +0300

    test

--- task7.py
+++ task7.py
@@ -13,82 +13,21 @@ def driver():
     return chr_driver
 
 
-def are_elements_present(driver, *args):
-    return len(driver.find_elements(*args)) == 1
+def test_check_stickers(driver):
+    list_goods = driver.find_elements(By.XPATH, "//ul[@class='listing-wrapper products']/li[*]")  # cписок всех товаров
+    for x in range(len(driver.find_elements(By.XPATH, "//ul[@class='listing-wrapper products']/li[*]"))):
+        assert len(list_goods[x].find_elements(By.XPATH,".//div[@class='image-wrapper']/div")) == 1  # внутри товара 1 элемент со стикером
 
 
-def test_blue_duck(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-most-popular']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/blue-duck-p-4']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
 
 
-def test_purple_duck(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-most-popular']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/purple-duck-p-5']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
 
 
-def test_red_duck(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-most-popular']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/red-duck-p-3']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
 
 
-def test_green_duck(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-most-popular']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/green-duck-p-2']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
 
 
-def test_yellow_duck(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-most-popular']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/yellow-duck-p-1']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker sale']")
-    assert len(elem_3) == 1
 
 
-def test_yellow_duck_campaigns(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-campaigns']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/yellow-duck-p-1']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker sale']")
-    assert len(elem_3) == 1
 
 
-def test_yellow_duck_latest(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-latest-products']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/yellow-duck-p-1']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker sale']")
-    assert len(elem_3) == 1
-
-
-def test_green_duck_latest(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-latest-products']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/green-duck-p-2']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
-
-
-def test_red_duck_latest(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-latest-products']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/red-duck-p-3']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
-
-
-def test_blue_duck_latest(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-latest-products']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/blue-duck-p-4']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
-
-
-def test_blue_purple_latest(driver):
-    elem_1 = driver.find_element(By.XPATH, "//*[@id='box-latest-products']")
-    elem_2 = elem_1.find_element(By.XPATH, ".//*[@href='http://localhost/litecart/en/rubber-ducks-c-1/purple-duck-p-5']")
-    elem_3 = elem_2.find_elements(By.XPATH, ".//div[@class='sticker new']")
-    assert len(elem_3) == 1
