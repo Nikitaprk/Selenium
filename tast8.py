6ce216f24a5b1971db741a01472e4f5da5fd10d3
Nikita <Narchuk96@gmail.com>
2023-09-18 21:53:27 +0300

    test

---
+++ tast8.py
@@ -0,0 +1,39 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture()
+def driver():
+    chr_driver = webdriver.Chrome()
+    chr_driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    chr_driver.maximize_window()
+    chr_driver.implicitly_wait(1)
+    chr_driver.find_element(By.XPATH, "//input[@type='text']").send_keys('admin')
+    chr_driver.find_element(By.XPATH, "//input[@type='password']").send_keys('admin')
+    chr_driver.find_element(By.NAME, 'remember_me').click()
+    chr_driver.find_element(By.NAME, 'login').click()
+    return chr_driver
+
+
+def test_check_abc(driver):
+    names = []
+    for x in driver.find_elements(By.XPATH, "//tr[@class='row']/td[5]"):
+        names.append(x.get_attribute('textContent'))  # перебираем все элементы, достаем названия стран и добавляем их в список
+    assert names == sorted(names)  # сравниваем наш список с таким же, но сортированным, узнаем был ли он отсортирован изначально
+
+
+def test_check_abc_inner(driver):
+    list_ = driver.find_elements(By.XPATH, "//tr[@class='row']/td[6]")  # ищем все зоны у стран
+    for x in range(len(list_)):
+        if int(list_[x].get_attribute('innerText')) > 0:  # если кол-во зон больше нуля
+            list_[x].find_element(By.XPATH, './following::a').click()  # то переходим далее по ссылке
+            names = []
+            for q in driver.find_elements(By.XPATH, "//td[3]/input[@type='hidden']"):
+                names.append(q.get_attribute('defaultValue'))  # засовываем все зоны в список
+            assert names == sorted(names)  # сравниваем его с им же, только отсортированным
+            driver.back()
+            list_ = driver.find_elements(By.XPATH, "//tr[@class='row']/td[6]") # заново ищем элементы
+
+
