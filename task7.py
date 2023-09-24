a207898cdf5065b90dbd1331a72945c2b524349e
Nikita <Narchuk96@gmail.com>
2023-09-24 15:47:20 +0300

    test

--- task7.py
+++ task7.py
@@ -13,7 +13,16 @@ def driver():
     return chr_driver
 
 
-def test_check_stickers2(driver):
-    list_goods = driver.find_elements(By.XPATH, "//div[@class='image-wrapper']")  # cписок всех товаров
+# def test_check_stickers2(driver):
+#     list_goods = driver.find_elements(By.XPATH, "//div[@class='image-wrapper']")  # cписок всех товаров
+#     for x in range(len(list_goods)):
+#         assert len(list_goods[x].find_elements(By.XPATH,".//div[starts-with(@class, 'sticker')]")) == 1  # внутри товара 1 элемент со стикером
+
+def test_check_stickers(driver):
+    list_goods = driver.find_elements(By.XPATH, "//img[@class='image']")  # cписок всех товаров
     for x in range(len(list_goods)):
-        assert len(list_goods[x].find_elements(By.XPATH,".//div[starts-with(@class, 'sticker')]")) == 1  # внутри товара 1 элемент со стикером
+        assert len(list_goods[x].find_elements(By.XPATH,"./parent::*/div[starts-with(@class, 'sticker')]")) == 1  # внутри товара 1 элемент со стикером
+
+# Выбрал семантически значимый признак. Вроде бы это class=image. Но так как этот класс находится на одном уровне
+# со стикером, то для поиска стикера пришлось усложнить локатор: поднимаемся к родителю, а затем уже ищем стикер
+# по другому поиск не работает
