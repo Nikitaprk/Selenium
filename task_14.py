8e64c0578a7c03368eb05b45afb921c99a2c7439
Nikita <Narchuk96@gmail.com>
2023-09-28 11:21:03 +0300

    test

---
+++ task_14.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture()
+def driver():
+    chr_driver = webdriver.Chrome()
+    chr_driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    chr_driver.maximize_window()
+    chr_driver.find_element(By.XPATH, "//input[@type='text']").send_keys('admin')
+    chr_driver.find_element(By.XPATH, "//input[@type='password']").send_keys('admin')
+    chr_driver.find_element(By.NAME, 'remember_me').click()
+    chr_driver.find_element(By.NAME, 'login').click()
+    return chr_driver
+
+
+def test_1(driver):
+    wait = WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, "//a[@class='button']").click()
+    for x in driver.find_elements(By.XPATH, "//form[*]//a[@target='_blank']"):
+        main_window = driver.current_window_handle  # пишем в переменную главную страницу
+        x.click()
+        current_window = driver.window_handles[-1]
+        driver.switch_to.window(current_window) # меняем текущее рабочее окно на только что открытое
+        wait.until(EC.presence_of_element_located((By.XPATH, "//h1"))) # ждем пока страница не прогрузится, ищем заголовок в новой странице
+        driver.close()  # закрываем выбранное рабочее окно
+        driver.switch_to.window(main_window)  # переключаемся на главное окно
